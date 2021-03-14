24eb2e4f5c241b544aee8274a14c23eac820e051
SherstnevVasiliy <root@kola-nn.com>
2021-03-14 23:48:01 +0300

    fix: implement min/max/avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
 exports.min = function min (array) {
-  return 0;
+  let result = 0;
+  if (array && array.length>0){
+    result = array[0];
+    for (let value of array) {
+      result = value < result ? value : result;
+    }
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = 0;
+  if (array && array.length>0){
+    result = array[0];
+    for (let value of array) {
+      result = value > result ? value : result;
+    }
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  if (array && array.length>0){
+    for (let value of array) {
+      result += value;
+    }
+    result = result / array.length;
+  }
+  return result;
 }
